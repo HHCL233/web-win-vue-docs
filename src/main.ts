67e11d28a08f48dc9b2fcd7bf750834839d38058
HHCL233 <XINGTAISHIJIAOXIQU@outlook.com>
2025-08-29 19:48:02 +0800

    fix: 初次修复Build无法打包其他vue

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { createApp } from 'vue'
 import router from './routes'
 import App from './App.vue'
+import Home from './home.vue'
+import Installation from './installation.vue'
 
 const app = createApp(App);
+const home = createApp(Home);
+const installation = createApp(Installation);
 
 app.use(router);
 
 app.mount("#app");
+home.mount("#home");
+installation.mount("#installation");
